ccf366ec1a2b65953f4aae65945028cd2fbfd0aa
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-01-03 16:14:58 +0530

    full example

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import { useFetchCountriesQuery } from 'generated/graphql'
 
 export const Countries: React.FC = () => {
-  return <div>Display countries here</div>
+  const { data, loading, error } = useFetchCountriesQuery()
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error fetching countries</p>
+
+  return (
+    <div>
+      <ul>
+        {data?.countries?.slice(0, 10).map((country, index) => (
+          <li key={index}>
+            {country?.name} ({country?.emoji})
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
